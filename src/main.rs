c0e3dfba805f7bf04f9786473ec00eec7f845b71
rinonguci <phatphamm01@gmail.com>
2024-06-12 21:44:06 +0700

    [feat]: impl repl

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
-use crate::parser::Parser;
-
-use ast::TNode;
-use evaluator::eval;
-use lexer::Lexer;
+use repl::run_repl;
 
 pub mod ast;
 pub mod evaluator;
 pub mod lexer;
 pub mod object;
 pub mod parser;
+pub mod repl;
 pub mod token;
 
 fn main() {
-    let input = r#"
-      (5 + 10 * 2 + 15 / 3) * 2 + -10;
-      (5 + 10 * 2 + 15 / 3) * 2 + -11;
-        "#;
-
-    let l = Lexer::new(input.to_string());
-    let mut p = Parser::new(l);
-    let program = p.parse_program();
-
-    println!("EQ === {:#?}", program);
-    println!("EQ === {:#?}", program.string());
-
-    println!("EQ === {:#?}", eval(Box::new(program.to_node())));
+    run_repl();
 }
---
+++ src/repl/mod.rs
@@ -0,0 +1,31 @@
+use std::io::{self, Write};
+
+use crate::{evaluator::eval, lexer::Lexer, parser::Parser};
+
+pub fn run_repl() {
+    println!("Welcome to the REPL CLI. Type 'exit' to quit.");
+
+    loop {
+        print!(">> ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        if input == "exit\n" {
+            break;
+        }
+
+        let l = Lexer::new(input.to_string());
+        let mut p = Parser::new(l);
+
+        let program = p.parse_program();
+        let x = eval(Box::new(program.to_node()));
+        println!("{:?}", x);
+    }
+
+    println!("Exit REPL!");
+}
